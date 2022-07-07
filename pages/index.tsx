88e76f7e0fa2d71924cc84df25f7a3a58f39ef69
Croustys <akos.barabas2002@gmail.com>
2022-07-07 17:17:26 +0200

    designed & coded frontpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
 import * as React from "react";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="title">
+          <h1>Quote Share</h1>
+        </div>
+        <div className="sub-title">
+          <span>Where you share</span>
+          <br />
+          <span>what's on your mind</span>
+        </div>
+        <div className="join-button">
+          <button id="join-now" type="button">
+            <span>Join now</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
